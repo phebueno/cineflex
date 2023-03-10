4cd006e864981c9f8ba084a4f41769530e5b418f
Phebueno <fcbgomes@gmail.com>
2023-03-10 16:10:17 -0300

    Botão retornar corrigido

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,17 @@
 import styled from "styled-components";
 import { AiOutlineArrowLeft } from "react-icons/ai";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const location = useLocation();
-  console.log(location);
+  const navigate = useNavigate();
   //Se a localização estiver na root ou na página /sucesso, não exibe o ícone
   return (
     <NavContainer>
       {(location.pathname === '/' || location.pathname==='/sucesso') ? (
         ""
       ) : (
-        <Link data-test="go-home-header-btn" to="/">
+        <Link data-test="go-home-header-btn" onClick={()=>navigate(-1)}>
           <AiOutlineArrowLeft />
         </Link>
       )}
